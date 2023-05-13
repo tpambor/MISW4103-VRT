b82d6da322b199818fe1ccf22de3a68b4bbc4ea1
Tim Pambor <t.pambor@uniandes.edu.co>
2023-05-13 14:09:06 +0200

    Load config from config.json

--- ResembleJS/index.js
+++ ResembleJS/index.js
@@ -1,12 +1,15 @@
 import compareImages from "resemblejs/compareImages.js";
 import * as fs from 'fs/promises';
 
-console.log("Hola ResembleJS");
-
 async function main() {
+  console.log("Hola ResembleJS");
+
+  const config = JSON.parse(await fs.readFile('./config.json'));
+
   var data = await compareImages(
     'ghost3/create-tag.cy.js/create-tag/esc07/00_SignIn_open.png',
-    'ghost4/create-tag.cy.js/create-tag/esc07/00_SignIn_open.png'
+    'ghost4/create-tag.cy.js/create-tag/esc07/00_SignIn_open.png',
+    config.resembleJS
   );
   await fs.writeFile("./diff.png", data.getBuffer());
 }

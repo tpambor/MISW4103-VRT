bc693ce5e5262e60b8b192b867a237c3ce915caf
Tim Pambor <t.pambor@uniandes.edu.co>
2023-05-13 13:31:53 +0200

    Basic demo of ResembleJS

--- ResembleJS/index.js
+++ ResembleJS/index.js
@@ -1,1 +1,14 @@
+import compareImages from "resemblejs/compareImages.js";
+import * as fs from 'fs/promises';
+
 console.log("Hola ResembleJS");
+
+async function main() {
+  var data = await compareImages(
+    'ghost3/create-tag.cy.js/create-tag/esc07/00_SignIn_open.png',
+    'ghost4/create-tag.cy.js/create-tag/esc07/00_SignIn_open.png'
+  );
+  await fs.writeFile("./diff.png", data.getBuffer());
+}
+
+await main();
